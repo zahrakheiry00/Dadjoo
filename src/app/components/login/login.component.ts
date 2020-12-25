4aa1086d14a9cf55fb5340bb953b2954072f5967
ZahraKheiry <zahra.kheiryashkuh00@gmail.com>
2020-12-25 04:10:32 -0800

    auth register and login

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,42 +1,65 @@
-import { Component, OnInit, ViewChild, Input, ElementRef, Output, EventEmitter } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  ViewChild,
+  Input,
+  ElementRef,
+  Output,
+  EventEmitter,
+} from '@angular/core';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
 import { UserService } from 'src/app/services/user.service';
-
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
-  username:any;
-  password:any;
+  // username:any;
+  // password:any;
+  invalidLogin = false;
 
-  constructor(private userService: UserService, private snackBar: MatSnackBar) { }
+  constructor(
+    private userService: UserService,
+    private snackBar: MatSnackBar,
+    private router: Router
+  ) {}
 
-  ngOnInit() {
+  ngOnInit() {}
 
-  }
+  // login() {
+  //   var inputs = {
+  //     "password": this.password,
+  //     "username": this.username
+  //   }
+  //   this.userService.login(inputs).subscribe(res => {
+  //     localStorage.setItem("token",res.token)
+  //     localStorage.getItem("token")
+  //     if (res.status == 0) {
+  //       this.snackBar.open(res.data, "", {
+  //         duration: 3000,
+  //       });
+  //     }
+  //     else {
+  //       this.snackBar.open(res.data, "", {
+  //         duration: 3000,
+  //       });
+  //     }
+  //   })
+  // }
 
-  login() {
+  loginClicked(username: any, password: any) {
     var inputs = {
-      "password": this.password,
-      "username": this.username
+      password: password,
+      username: username,
+    };
+    if (this.userService.login(inputs)) {
+      //console.log(this.userService.login(inputs));
+      this.router.navigateByUrl('test');
+    } else {
+      this.invalidLogin = true;
     }
-    this.userService.login(inputs).subscribe(res => {
-      localStorage.setItem("token",res.token)
-      localStorage.getItem("token")
-      if (res.status == 0) {
-        this.snackBar.open(res.data, "", {
-          duration: 3000,
-        });
-      }
-      else {
-        this.snackBar.open(res.data, "", {
-          duration: 3000,
-        });
-      }
-    })
   }
-
 }
--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.scss']
+  styleUrls: ['./register.component.scss'],
 })
 export class RegisterComponent implements OnInit {
+  invalidLogin = false;
 
-  constructor() { }
+  constructor(private userService: UserService) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  registerClicked(username: any, email: any, phoneNumber: any, password: any) {
+    var inputs = {
+      username: password,
+      email: username,
+      phoneNumber: phoneNumber,
+      password: password,
+    };
+    if (this.userService.register(inputs)) {
+      //navigate to next page
+    } else {
+      this.invalidLogin = true;
+    }
   }
-
 }
